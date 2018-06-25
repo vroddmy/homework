bb88bac0b2644b6eef7bf0e8db0655839a2c8a51
vroddmy <35878859+vroddmy@users.noreply.github.com>
2018-06-25 17:09:21 +0300

    me

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+	$myName = "Влад";
+	$myAge = 18;
+	$myEmail = "vroddmy@gmail.com";
+	$myCity = "Smolensk";
+	$aboutMe = "Учусь програмировать";
+?>
+<?php 
+	Имя: <? echo "$myName ", "<br>">;
+	Возраст: <? echo "$myAge", "<br>">;
+	e-mail: <? echo "$myEmail", "<br>">;
+	Город:  <? echo "$myCity", "<br>">;
+	О себе: <? echo "$aboutMe","<br>">;
+ ?>
