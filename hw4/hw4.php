6dea0fdb3043f2cd87caafb7b5fc2b7cc6c5f7f4
vroddmy <35878859+vroddmy@users.noreply.github.com>
2018-07-14 23:46:24 +0300

    4

--- hw4/hw4.php
+++ hw4/hw4.php
@@ -4,24 +4,32 @@ $link = 'http://api.openweathermap.org/data/2.5/weather';
 $apiKey = 'e7425706bcd4490ebe5d6fef880070b0';
 $city = 'Smolensk';
 $units = 'metric';
-$apiURL = "{$link}?q={$city}&units={$units}&appid={$apiKey}";
+$apiURL = '{$link}?q={$city}&units={$units}&appid={$apiKey}';
 
 
+// $content = file_get_contents("{$link}?q={$city}&units={$units}&appid={$apiKey}");
+//$content = file_get_contents("http://api.openweathermaporg./data/2.5/weather?q=Smolensk,ru&appid=e7425706bcd4490ebe5d6fef880070b0");
 $content = file_get_contents("{$link}?q={$city}&units={$units}&appid={$apiKey}") or exit('Не удалось получить данные');
+// $content = file_get_contents($link);
 
-$location = "Current weather in Smolensk";
-$weather = "Weather: ";
-$current_temp = "Current temperature, °С: ";
-$temp_min = "Minimum temperature, °С: ";
-$temp_max = "Maximum temperature, °С: ";
-$wind_power = "Wind speed, m/s: ";
-$humidity = "Humidity, %: ";
-$pressure = "Pressure, mm Hg: ";
-$result = json_decode($content,true);
-$icon = "http://openweathermap.org/img/w/".$result["weather"][0]["icon"].".png";
+
+$location = 'Current weather in Smolensk';
+$weather = 'Weather: ';
+$current_temp = 'Current temperature, °С: ';
+$temp_min ='Minimum temperature, °С: ';
+$temp_max = 'Maximum temperature, °С: ';
+$wind_power = 'Wind speed, m/s: ';
+$humidity = 'Humidity, %: ';
+$pressure = 'Pressure, mm Hg: ';
+$result = json_decode($content, true) or exit('Ошибка декодирования json');
+function checkData($data){
+	if (empty($data)) { return 'не удалось получить данные';
+}
+return $data;
+}
+
+$icon = "http://openweathermap.org/img/w/".$result['weather'][0]['icon'].'.png';
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
